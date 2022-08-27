0588b32a264c9d3282578c4727f8dcfe1ab52e24
arunarvindh05 <arunarvindh05@gmail.com>
2022-08-27 23:28:33 +0530

    Adding all codes for portfolio

---
+++ js/script.js
@@ -0,0 +1,114 @@
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme)?'dark':'light'
+const getCurrentIcon = () => document.body.classList.contains(iconTheme)?'uil-moon':'uil-sun'
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark'?'add':'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon'?'add':'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+   
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+   
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+} 
+
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+} 
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+new Typewriter('#typewriter', {
+  strings: ['Arun Arvindh', 'Web Developer', 'Computer Science Engineer', ''],
+  autoStart: true,
+  loop: true,
+  cursor: "|"
+});
+
+
+var swiper = new Swiper(".blog-slider", {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel:{
+        invert: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".blog-slider__pagination",
+      clickable: true,
+    },
+    keyboard: true,
+  });
+
+
+function scrollUp(){
+    const scrollup = document.getElementById('scroll-up');
+    // When the scroll higher than 560 viewpoint /height , then the scroll up icon showld appear and on clicking should reach top of the page
+    if(this.scrollY >= 560) {
+        scrollup.classList.add('show-scroll');
+    }
+    else {
+        scrollup.classList.remove('show-scroll')
+    }
+}
+window.addEventListener('scroll', scrollUp)
+
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+
+}
+window.addEventListener('scroll', scrollActive)
+
+
